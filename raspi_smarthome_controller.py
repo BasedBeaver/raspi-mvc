385c31b9e78a996ff49df9844be5a78784e73725
Daniel <daniel.dimitric91@gmail.com>
2018-12-23 16:58:04 +0100

    turn on computer button added

--- raspi_smarthome_controller.py
+++ raspi_smarthome_controller.py
@@ -4,7 +4,10 @@ import tkinter as tk
 class Model:
 
     def desk_lamp(self):
-        print("Add ethernet package")
+        print("Turn On/Off desk lamp")
+
+    def magic_package(self):
+        print("Turn On computer")
 
 
 class View(tk.Frame):
@@ -12,15 +15,20 @@ class View(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
-        main_canvas = tk.Canvas(parent, bg="dark green")
-        main_canvas.pack(fill=tk.BOTH, expand=True)
+        self.main_canvas = tk.Canvas(parent, bg="dark green")
+        self.main_canvas.pack(fill=tk.BOTH, expand=True)
 
-        desk_lamp_button = tk.Button(main_canvas, text="Desk Lamp ON/OFF", command=self.desk_lamp)
-        desk_lamp_button.pack(side="right")
+        self.desk_lamp_button = tk.Button(self.main_canvas, text="Desk Lamp ON/OFF", command=self.desk_lamp)
+        self.desk_lamp_button.pack(side="right", expand=True, fill=tk.BOTH)
+
+        self.computer_button = tk.Button(self.main_canvas, text="Turn On Computer", command=self.magic_package)
+        self.computer_button.pack(side="left", expand=True, fill=tk.BOTH)
 
     def desk_lamp(self):
-        print("YAY")
         c.desk_lamp()
+
+    def magic_package(self):
+        c.magic_package()
 
         # <create the rest of your GUI here>
 
@@ -38,8 +46,10 @@ class Controller:
         self.root.mainloop()
 
     def desk_lamp(self):
-        print("c.yay")
         self.model.desk_lamp()
+
+    def magic_package(self):
+        self.model.magic_package()
 
 
 if __name__ == "__main__":
