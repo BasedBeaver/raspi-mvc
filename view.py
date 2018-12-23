94beedb8dcd289bc1d84518fc526c1ee628c5bfe
Daniel <daniel.dimitric91@gmail.com>
2018-12-23 16:04:44 +0100

    got rolling from skeleton code

--- view.py
+++ view.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import controller as c
 
 
 class MainApplication(tk.Frame):
@@ -6,11 +7,14 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
+        main_canvas = tk.Canvas(parent, bg="dark green")
+        main_canvas.pack(fill=tk.BOTH, expand=True)
+
         # <create the rest of your GUI here>
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("800x600")
-    MainApplication(root).pack(side="top", fill="both", expand=True)
+    # root.geometry("800x600")
+    MainApplication(root).pack(fill=tk.BOTH, expand=False)
     root.mainloop()
